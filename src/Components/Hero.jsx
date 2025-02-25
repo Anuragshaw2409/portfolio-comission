6b3f27ca00b1eb2059ed1149577f4e37322c2f2e
Anuragshaw2409 <shawanurag2012@gmail.com>
2025-02-26 00:31:51 +0530

    updated1

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
         <div className='flex flex-col gap-4 md:text-left text-center mb-6'>
             <h1 className='text-white text-6xl font-thin'>Hi, I am </h1>
             <h1 className='text-white text-7xl font-thin '>Dr. Pradeep Kothiyal</h1>
-            <h2 className='text-white font-thin'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis provident dignissimos voluptatibus, exercitationem neque maxime! Et accusamus commodi quisquam. Culpa velit laboriosam illum quidem, tempora eum sit praesentium itaque ullam officia tempore, necessitatibus consectetur, accusantium rerum soluta blanditiis? Sint repellendus reiciendis vel iusto voluptatem enim tempora quae nesciunt fuga necessitatibus?</h2>
+            <h2 className='text-white font-thin'>I am a dedicated researcher, educator, and industry professional specializing in nanocomposite materials, manufacturing engineering, and metal matrix nanocomposites. With a Ph.D. from Uttarakhand Technical University, I have contributed extensively to academia and industry, currently serving as an Associate Professor at JB Institute of Technology, Dehradun. My expertise includes AutoCAD, Taguchi Optimization, and RSM, with a strong focus on microstructural evolution and fracture behavior.</h2>
         </div>
         <div className='flex justify-center ' > 
             <img src={hero_image} alt="" className='w-96 rounded-3xl shadow-violet-900 shadow-2xl' title='Dr. Pradeep Kothiyal'/>
